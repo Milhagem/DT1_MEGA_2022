236461e22c675de7094e3eaf8131708f74194d38
Bruno Andrade <bruno.andrade.ufmg@gmail.com>
2022-11-14 13:50:27 -0300

    atualizacao nos comentarios e no uso de variaveis
    1 - tirei os acentos dos comentários (maior compatibilidade entre editores), ainda deve ter alguns que passaram despercebidos.
    2 - mudei alguns comentarios para explicacao ficar mais clara
    3 - na definição da variavel hallX_estado antes se usava o numero do pino  e nao HALLX que havia sido definido no começo do código, foi corrigido
    4 - Definicao de PWM_MAX para alterar rapidamente o pwm maximo da rampa

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,50 @@
 /*
- *  O seguinte código é utilizado num arduino MEGA, cujo papel é a de um controlador de um motor BLDC
+ *  O seguinte código eh utilizado num arduino MEGA, cujo papel eh a de um controlador de um motor BLDC
  *  O Arduino recebe sinal dos sensores de efeito hall e comuta os mosfets de acordo com os valores lidos
- *  O motor é ligado apenas sob certas condições de interruptores e fins de curso
- *  A entrada para o motor é uma entrada rampa, com incremento gradual
+ *  O motor eh ligado apenas sob certas condicoes de interruptores e fins de curso
+ *  A entrada para o motor eh uma entrada rampa, com incremento gradual
  */
 
 // Biblioteca Arduino.h adicionada para aumentar compatibilidade com outros editores
 #include <Arduino.h>
 
-// Relação de portas Shield Controlador -> Powerdriver
+// Relação de portas Shield Controlador -> Powerdriver (sinais de saida para os gates dos mosfets)
 #define MOSFET_A_HIGH 5
 #define MOSFET_B_HIGH 6
 #define MOSFET_C_HIGH 7
+
 #define MOSFET_A_LOW 9
 #define MOSFET_B_LOW 10
 #define MOSFET_C_LOW 11
 
+// Duty cycle do PWM maximo no gate
+#define PWM_MAX 99
+
 // Portas definidas MEGA
-#define hallA 4           // Entrada hall A do motor
-#define hallB 3           // Entrada hall B do motor
-#define hallC 2           // Entrada hall C do motor
-#define botao1_pedal 15   // Acionado pelo pedal de aceleração. Fim de Curso Dianteiro. Entrada do botão 1 de aceleração (Começando a pressionar)
-#define botao2_pedal 16   // Acionado pelo pedal de aceleração. Fim de Curso Traseiro. Entrada do botão 2 de aceleração (Completamente pressionado)
+#define hallA 4           // Entrada do sinal do sensor de efeito hall A do motor
+#define hallB 3           // Entrada do sinal do sensor de efeito hall B do motor
+#define hallC 2           // Entrada do sinal do sensor de efeito hall C do motor
+
+#define botao1_pedal 15   // Acionado pelo pedal de aceleracao. Fim de Curso [Dianteiro]. Entrada do botão 1 de aceleracao (Pedal meio pressionado)
+#define botao2_pedal 16   // Acionado pelo pedal de aceleracao. Fim de Curso [Traseiro]. Entrada do botão 2 de aceleracao (Pedal Completamente pressionado)
+
 #define chave_controle 19 // Configura o valor inicial do PWM nos mosfets. Sendo o valor mais baixo para arrancada e o maior para ao longo da corrida
-#define acelerador 8      // Para ligar o carro sem apertar o pedal. Alternativa para não precisar apertar o pedal até o fundo
+#define acelerador 8      // Para ligar o carro sem apertar o pedal. Alternativa para não precisar apertar o pedal ate o fundo
 #define sinal_corrente 22 // Desativa a alimentação se a corrente estiver muito alta
 
-int hallA_estado;                                  // Estado do hall A
-int hallB_estado;                                  // Estado do hall B
-int hallC_estado;                                  // Estado do hall C
+int hallA_estado;                                  // Estado do sensor de efeito hall A do motor BLDC
+int hallB_estado;                                  // Estado do sensor de efeito hall B do motor BLDC
+int hallC_estado;                                  // Estado do sensor de efeito hall C do motor BLDC
 int hall_val = 1;                                  // Estado dos tres halls (Fase do motor)
 int botao1_pedal_anterior = 3;                     // Estado anterior do fim de curso dianteiro
 int botao2_pedal_anterior = 3;                     // Estado anterior do fim de curso traseiro
-int estado_controle, estado_controle_ant = LOW;    // Serve para zerar o dutyCycle para o início da rampa
+int estado_controle, estado_controle_ant = LOW;    // Serve para zerar o duty cycle para o início da rampa
 unsigned long tempo_Atual = 0, tempo_Anterior = 0; // Servem para incrementar o PWM depois de x microsegundos
 
-// Variáveis Relevantes
+// Variaveis Relevantes
 int incremento_rampa = 0; // Duty cycle em % , de 0 a 100%
 int inicio_rampa = 0;     // Valor inicial da rampa
-int pwwm = 0;             // Armazena o PWM que irá para os MOSFET's
+int pwwm = 0;             // Armazena o PWM que ira para os MOSFETs
 
 void setup()
 {
@@ -49,18 +55,18 @@ void setup()
   pinMode(sinal_corrente, INPUT);
 
   // Saídas do Arduino MEGA
-  // Saídas para ativação MOSFET's do inversor trifásico
+  // Saídas para ativação MOSFETs do inversor trifasico
   // Saídas digitais HIGH SIDE
   pinMode(MOSFET_A_HIGH, OUTPUT);
   pinMode(MOSFET_B_HIGH, OUTPUT);
   pinMode(MOSFET_C_HIGH, OUTPUT);
 
-  // Saídas PWM LOW SIDE
+  // Saidas PWM LOW SIDE
   pinMode(MOSFET_A_LOW, OUTPUT);
   pinMode(MOSFET_B_LOW, OUTPUT);
   pinMode(MOSFET_C_LOW, OUTPUT);
 
-  // Coloca as saídas dos MOSFET's em low
+  // Coloca as saidas dos MOSFETs em low
   PORTE &= ~(1 << PORTE3); // pin 5
   PORTH &= ~(1 << PORTH3); // pin 6
   PORTH &= ~(1 << PORTH4); // pin 7
@@ -68,31 +74,31 @@ void setup()
   analogWrite(10, 0);
   analogWrite(11, 0);
 
-// Define a frequência do PWM para os MOSFET's
-// Seta Timer 1 (1B) para 490 Hz para frequência do PWM (pino 11)
-// Seta Timer 2 (2B) para 980 Hz para frequência do PWM (pino 9 e 10)
-  int myEraser = 7;            // cria uma variável com valor 7 (111 em binário) para modificar os registradores dos pinos 9, 10 e 11
-  TCCR1B &= ~myEraser;         // usa operações AND e NOT, usando o valor de myEraser para ZERAR os três primeiros dígitos de TCCR1B (pino 11)
-  TCCR2B &= ~myEraser;         // usa operações AND e NOT, usando o valor de myEraser para ZERAR os três primeiros dígitos de TCCR2B (pinos 9 e 10)
+// Define a frequencia do PWM para os MOSFETs
+// Seta Timer 1 (1B) para 490 Hz para frequencia do PWM (pino 11)
+// Seta Timer 2 (2B) para 980 Hz para frequencia do PWM (pino 9 e 10)
+  int myEraser = 7;            // cria uma variavel com valor 7 (111 em binario) para modificar os registradores dos pinos 9, 10 e 11
+  TCCR1B &= ~myEraser;         // usa operacoes AND e NOT, usando o valor de myEraser para ZERAR os tres primeiros digitos de TCCR1B (pino 11)
+  TCCR2B &= ~myEraser;         // usa operacoes AND e NOT, usando o valor de myEraser para ZERAR os tres primeiros digitos de TCCR2B (pinos 9 e 10)
   int myPrescaler = 3;         // essa variável pode ser um número entre 1 e 6, de modo que cada um corresponde a uma frequência, dependendo do registrador. Para o registrador 1B, o número 3 é 490Hz. Para o 2B, é 980Hz.
-  TCCR1B |= myPrescaler;       // usa a operação OR para substituir os valores dos três últimos dígitos para o valor de 3 (011)
-  TCCR2B |= myPrescaler;       // usa a operação OR para substituir os valores dos três últimos dígitos para o valor de 3 (011)
+  TCCR1B |= myPrescaler;       // usa a operacao OR para substituir os valores dos tres ultimos digitos para o valor de 3 (011)
+  TCCR2B |= myPrescaler;       // usa a operacao OR para substituir os valores dos tres ultimos digitos para o valor de 3 (011)
 
 }
 void loop() {
     tempo_Atual = millis(); 
     estado_controle = digitalRead(chave_controle);
   
-    // Se houver mudança da chave controle, então a rampa é "zerada". Voltando a incrementar do zero
+    // Se houver mudanca da chave controle, entao a rampa eh "zerada". Voltando a incrementar do zero
     if(estado_controle != estado_controle_ant){
         incremento_rampa = 0;
         estado_controle_ant = estado_controle;
     }
   
-    // Configura o valor do início da rampa de acordo com o selecionado na chave_controle
-    // O valor mais baixo é para a arrancada, e o mais alto é para a pista
+    // Configura o valor do inicio da rampa de acordo com o selecionado na chave_controle
+    // O valor mais baixo eh somente para a arrancada, e o mais alto eh para a pista
     // Os valores podem mudar conforme variar o peso do carro e do piloto
-    if(digitalRead(chave_controle)==HIGH){
+    if(digitalRead(chave_controle) == HIGH){
        inicio_rampa = 25;
     }
     else{
@@ -102,7 +108,7 @@ void loop() {
   
     // Liga o carro se o pedal estiver completamente pressionado. Ou se o acelerador estiver ligado
     if( ( (digitalRead(botao2_pedal)== LOW) && (digitalRead(botao1_pedal)== HIGH) ) || (digitalRead(acelerador) == LOW) ){
-        if (incremento_rampa < 99 - inicio_rampa){            // Se o PWM sobre os MOSFET's ainda é menor do que 90%
+        if (incremento_rampa < PWM_MAX - inicio_rampa){            // Se o duty cycle do PWM sobre o gate dos MOSFETs ainda eh menor do que PWM_MAX
             if ((tempo_Atual - tempo_Anterior) >= 70){  // Incrementa a cada 70 milisegundos
             incremento_rampa+=1;
             tempo_Anterior = tempo_Atual;
@@ -112,23 +118,23 @@ void loop() {
     }
     else{ 
           incremento_rampa = 0;
-          pwwm = map(incremento_rampa, 0, 100, 0, 255); // Não entendi porque colocaram essa variável sendo que podia só por o                                                           // valor zero, por mim acho que pode por o zero
+          pwwm = map(incremento_rampa, 0, 100, 0, 255); // Nao entendi porque colocaram essa variável sendo que podia só por o                                                           // valor zero, por mim acho que pode por o zero
     }
 
-  hallA_estado = digitalRead(4); // variavel recebe o valor do sensor Hall A
-  hallB_estado = digitalRead(3); // variavel recebe o valor do sensor Hall B
-  hallC_estado = digitalRead(2); // variavel recebe o valor do sensor Hall C
+  hallA_estado = digitalRead(hallA); // variavel recebe o valor do estado do sensor de efeito Hall A do motor
+  hallB_estado = digitalRead(hallB); // variavel recebe o valor do estado do sensor de efeito Hall B do motor
+  hallC_estado = digitalRead(hallC); // variavel recebe o valor do estado do sensor de efeito Hall C do motor
 
   hall_val = (hallA_estado) + (2 * hallB_estado) + (4 * hallC_estado); // converte o valor dos 3 sensores Hall para números decimais
 
   switch (hall_val)
   {
     /*
-     * Funcionando em sentido horário, esses valores estão descrito no TCC que está na biblioteca do Notion sobre o motor BLDC
+     * Funcionando em sentido horario, esses valores estão descritos no TCC que esta na biblioteca do Notion sobre o motor BLDC
      *
      * --------------------------------------------------------------------------------------------------------------------------
      *
-     * Sequência das fases para os mosfets ligados:
+     * Sequencia das fases para os mosfets ligados:
      * BL AH
      * BL CH
      * AL CH
@@ -138,15 +144,15 @@ void loop() {
      *
      * --------------------------------------------------------------------------------------------------------------------------
      *
-     * PORTD : muda o estado dos pinos usando registradores, mais rápido que o analog/digital Write
-     * PORTD = B001xxx00;             Saída desejada para pinos 0-7; xxx refere-se às entradas Hall e pinos 0 e 1 do serial, que
-     *não devem ser alteradas
-     * Operação AND para atribuir 000 nas saídas HIGH SIDE (zerar valor anterior) e OR para alterar o valor da desejado
+     * PORTD : muda o estado dos pinos usando registradores, mais rapido que o analog/digital Write
+     * PORTD = B001xxx00;             Saída desejada para pinos 0-7; xxx refere-se as entradas Hall e pinos 0 e 1 do serial, que
+     * nao devem ser alteradas
+     * Operacao AND para atribuir 000 nas saidas HIGH SIDE (zerar valor anterior) e OR para alterar o valor da desejado
      *
      * --------------------------------------------------------------------------------------------------------------------------
      *
-     * É importante garantir que todos os transistores estejam abertos (ou ao menos 5 deles) para fazer a comutação dos MOSFET's
-     * Caso isso não seja feito, há o risco de causar um curto-circuito na bateria
+     * Eh importante garantir que todos os transistores estejam abertos (ou ao menos 5 deles) para fazer a comutacao dos MOSFETs
+     * Caso isso nao seja feito, existe o risco de causar um curto-circuito na bateria
      *
      * --------------------------------------------------------------------------------------------------------------------------
      */
